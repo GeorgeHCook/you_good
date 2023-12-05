d5673ca8e289051ca73b4f82d7f31f520105ad4e
George Cook <georgehhcook@gmail.com>
2023-12-05 11:56:15 +0000

    route and controller done, simple form to test

--- app/controllers/check_ins_controller.rb
+++ app/controllers/check_ins_controller.rb
@@ -1,2 +1,23 @@
 class CheckInsController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def new
+    @check_in = CheckIn.new
+  end
+
+  def create
+    @check_in = CheckIn.new(check_in_params)
+    @check_in.user = current_user
+    if @check_in.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def check_in_params
+    params.require(:check_in).permit(:score, :details)
+  end
 end
