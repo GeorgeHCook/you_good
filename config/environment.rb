8fd740ccc8a9158b39983bb5185879d11ca41f89
francisco vitor <francescptt3@gmail.com>
2023-12-05 14:49:03 +0000

    cloudinary

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the Rails application.
 require_relative "application"
 
-CLOUDINARY_URL=cloudinary://298522699261255:***********************8@Qa1ZfO4syfbOC
-
 # Initialize the Rails application.
 Rails.application.initialize!
