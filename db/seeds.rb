55dab5b3073e9f65b744de97deeeb081d2a32924
George Cook <georgehhcook@gmail.com>
2023-12-08 14:25:42 +0000

    cleaning

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@ puts 'Creating Users & Check ins...'
 require "faker"
 require "rspotify"
 
-names = ["George", "Francisco", "Michael", "Jamie", "Thomas"]
-5.times do |i|
+names = ["George", "Francisco", "Michael", "Jamie", "Thomas", "Constantine", "Will", "Havish", "Dareos", "Arbi", "Luca"]
+11.times do |i|
   email = "#{names[i - 1]}@test.com"
   first_name = names[i - 1].to_s
   password = "123456"
@@ -21,13 +21,6 @@ names = ["George", "Francisco", "Michael", "Jamie", "Thomas"]
 
   puts "created #{user.first_name}"
 end
-
-Medium.create!(title: "Body Appreciation Meditation • Trauma-Sensitive Mindfulness", category: "Podcast", media_type: "Audio", media_url: "https://soundcloud.com/tsm-podcast/body-appreciation-meditation-trauma-sensitive-mindfulness?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" , artwork_url: "https://i1.sndcdn.com/avatars-000630945204-w6p0si-t500x500.jpg")
-Medium.create!(title: "Khalid - Therapy", category: "Music", media_type: "Audio", media_url: "https://soundcloud.com/thegreatkhalid/therapy?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" , artwork_url: "https://i1.sndcdn.com/artworks-Wqkma6LcaqFd-0-t500x500.jpg")
-Medium.create!(title: "Relaxing Music for Stress Relief", category: "Music", media_type: "Audio", media_url: "https://soundcloud.com/spiritualmoment/relaxing-music-for-stress-relief-calm-music-for-sleep-meditation-healing-therapy-study-spa-yoga?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" , artwork_url: "https://i1.sndcdn.com/artworks-000340497561-i2zpyx-t500x500.jpg")
-Medium.create!(title: "Eddie Nestor | Male Mental Health & Suicide", category: "Podcast", media_type: "Audio", media_url: "https://soundcloud.com/eddie-nestor-558883944/eddie-nestor-male-mental-health-suicide?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" , artwork_url: "https://i1.sndcdn.com/artworks-000473849547-2jz7n9-t500x500.jpg")
-Medium.create!(title: "Mindfulness", category: "Music", media_type: "Audio", media_url: "https://soundcloud.com/yoga-relax/mindfulness?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing", artwork_url: "https://i1.sndcdn.com/artworks-3TdkZ53Y0Ist-0-t500x500.jpg")
-
 
 puts 'finished'
 
