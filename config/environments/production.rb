fdcd789b99bc4b8db8e322faf38fc1e6facc01a5
George Cook <georgehhcook@gmail.com>
2023-12-08 15:05:34 +0000

    redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,9 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.active_storage.service = :cloudinary
+
+
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
