6a9b1e30c9323397bf6cb6cc28625852b573d868
George Cook <georgehhcook@gmail.com>
2023-12-13 16:17:23 +0000

    debugged

--- app/models/check_in.rb
+++ app/models/check_in.rb
@@ -3,8 +3,8 @@ class CheckIn < ApplicationRecord
   has_many :media, dependent: :destroy
   has_one_attached :photo
   before_save :set_details_content
-  before_save :video
-  before_save :music
+  after_create :video
+  after_create :music
   # before_create unless: video_content.blank? do
   #   video
   # end
@@ -18,8 +18,7 @@ class CheckIn < ApplicationRecord
       model: "gpt-3.5-turbo",
       messages: [{ role: "user", content: "take the mental health score #{score} out of 10, and the input #{details}, and generate key words for a spotify music search, limit to 4 words, just reply with the keywords without absolutely no addition comments. Do not include the word 'keywords'"}]
     })
-    self.music_content = chaptgpt_music_response["choices"][0]["message"]["content"]
-    music
+    chaptgpt_music_response["choices"][0]["message"]["content"]
   end
 
   def music_content
@@ -36,8 +35,7 @@ class CheckIn < ApplicationRecord
       model: "gpt-3.5-turbo",
       messages: [{ role: "user", content: "take the mental health score #{score} out of 10, and the input #{details}, and generate 4 key words for a youtube video search for upbeat yoga or meditation and other activities and videos with corresponding energy, just reply with the keywords, separated with an plus sign"}]
     })
-    self.video_content = chaptgpt_video_response["choices"][0]["message"]["content"]
-    video
+    chaptgpt_video_response["choices"][0]["message"]["content"]
   end
 
   def video_content
@@ -58,48 +56,49 @@ class CheckIn < ApplicationRecord
   end
 
   def video
-    keywords = self.video_content
+    keywords = set_video_content
     response = HTTParty.get("https://www.googleapis.com/youtube/v3/search?key=#{ENV["YOUTUBE_API_KEY"]}&q=#{keywords}&part=snippet&maxResults=10&type=video")
     unless response["error"]
-      response["items"].map do |video|
-        Medium.create!(check_in: self, video_id: video["id"]["videoId"], media_type: "video")
+      response["items"].map do |result|
+        Medium.create!(check_in: self, video_id: result["id"]["videoId"], media_type: "video")
         # video["id"]["videoId"]
       end
     end
   end
 
   def music
-    keywords = self.music_content
+    keywords = set_music_content
     RSpotify.authenticate(ENV.fetch("SPOTIFY_ID"), ENV.fetch("SPOTIFY_SECRET"))
     # begin
-      if (1..2).include?(self.score)
-        tracks = RSpotify::Track.search(keywords)
-        results = tracks.select do |track|
-          audio_features = track.audio_features
-          audio_features.valence >= 0.007 && audio_features.energy <= 0.3
-        end
-      elsif (3..5).include?(self.score)
-        tracks = RSpotify::Track.search(keywords)
-        results = tracks.select do |track|
-          audio_features = track.audio_features
-          audio_features.valence >= 0.007 && audio_features.energy <= 0.5
-        end
-      elsif (6..7).include?(self.score)
-        tracks = RSpotify::Track.search(keywords)
-        results = tracks.select do |track|
-          audio_features = track.audio_features
-          audio_features.valence >= 0.007 && audio_features.energy <= 0.7
-        end
-      elsif (8..10).include?(self.score)
-        tracks = RSpotify::Track.search(keywords)
-        results = tracks.select do |track|
-          audio_features = track.audio_features
-          audio_features.valence >= 0.007 && audio_features.energy <= 0.8
-        end
+    if (1..2).include?(self.score)
+      tracks = RSpotify::Track.search(keywords)
+      results = tracks.select do |track|
+        audio_features = track.audio_features
+        audio_features.valence >= 0.007 && audio_features.energy <= 0.3
       end
-      results.map do |track|
-        Medium.create!(check_in: self, music_id: track.id, media_type: "music")
+    elsif (3..5).include?(self.score)
+      tracks = RSpotify::Track.search(keywords)
+      results = tracks.select do |track|
+        audio_features = track.audio_features
+        audio_features.valence >= 0.007 && audio_features.energy <= 0.5
       end
+    elsif (6..7).include?(self.score)
+      tracks = RSpotify::Track.search(keywords)
+      results = tracks.select do |track|
+        audio_features = track.audio_features
+        audio_features.valence >= 0.007 && audio_features.energy <= 0.7
+      end
+    elsif (8..10).include?(self.score)
+      tracks = RSpotify::Track.search(keywords)
+      results = tracks.select do |track|
+        audio_features = track.audio_features
+        audio_features.valence >= 0.007 && audio_features.energy <= 0.8
+      end
+    end
+    results.map do |track|
+      Medium.create!(check_in: self, music_id: track.id, media_type: "music")
+      puts "created Medium for #{track}"
+    end
     # rescue RestClient::ExceptionWithResponse, RestClient::TooManyRequests, Exception => e
     #   RSpotify.authenticate(ENV.fetch("SPOTIFY_ID2"), ENV.fetch("SPOTIFY_SECRET2"))
     #   retry
