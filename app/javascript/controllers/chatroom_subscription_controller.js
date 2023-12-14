2cfe328c09bf08752c1bcbbd70147db93bf9d222
George Cook <georgehhcook@gmail.com>
2023-12-14 14:28:15 +0000

    done

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 import { createConsumer } from "@rails/actioncable";
 export default class extends Controller {
-  static values = { chatroomId: Number };
+  static values = { chatroomId: Number, currentUserId: Number };
   static targets = ["messages"];
   connect() {
     this.channel = createConsumer().subscriptions.create(
@@ -32,8 +32,28 @@ export default class extends Controller {
   }
   // Private method to insert a new message, scroll to the bottom, and reset the form
   insertMessageAndScrollDown(data) {
-    this.messagesTarget.insertAdjacentHTML("beforeend", data);
+    const currentUserIsSender = this.currentUserIdValue === data.sender_id;
+    const messageElement = this.buildMessageElement(currentUserIsSender, data.message);
+    this.messagesTarget.insertAdjacentHTML("beforeend", messageElement);
     this.scrollToBottom();
+  }
+
+  buildMessageElement(currentUserIsSender, message) {
+    return `
+      <div class="message-row d-flex ${this.justifyClass(currentUserIsSender)}">
+        <div class="${this.userStyleClass(currentUserIsSender)}">
+          ${message}
+        </div>
+      </div>
+    `
+  }
+
+  justifyClass(currentUserIsSender) {
+    return currentUserIsSender ? "justify-content-end" : "justify-content-start"
+  }
+
+  userStyleClass(currentUserIsSender) {
+    return currentUserIsSender ? "sender-style" : "receiver-style"
   }
   // Private method to scroll to the bottom
   scrollToBottom() {
